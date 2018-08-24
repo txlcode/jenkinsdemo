60de32dce6872e6ed5f33a79ec07ab976a078547
tanxiaoliang <tanxiaoliang@cdsb.com>
2018-08-24 15:15:35 +0800

    test

---
+++ hello.java
@@ -0,0 +1,9 @@
+@RestController
+public class Hello {
+
+    @RequestMapping("/")
+    public String sayHello(){
+        //修改返回的字符串的内容
+        return "abcdefg. Hello jenkins, " + new Date();
+            }
+ }
